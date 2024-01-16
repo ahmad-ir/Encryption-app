9c8a7950a6d83a98b29dc109155b1fa98190ec22
Ahmad <amiriahmadir@gmail.com>
2024-01-16 21:21:45 +0100

    More UI controls added

--- main.py
+++ main.py
@@ -7,14 +7,33 @@ def main_screen():
     screen.geometry("375x398")
     screen.title("Encryption App")
 
-    label = Label(
+    lable1 = Label(
         text="Enter text for encryption and decryption",
         fg="black", font = ("calbri", 13))
     
-    label.place(x=10, y=10)
+    lable1.place(x=10, y=10)
 
     text1 = Text(font= "Robote 20", bg= "white", relief= GROOVE, wrap= WORD)
     text1.place(x=10, y=50, width= 355, height= 100)
+
+    label2 = Label(text= "Enter secret key for encryption and decryption", 
+                   fg= "black", font= ("calibri", 13))
+    label2.place(x= 10, y= 170)
+
+    code = StringVar()
+    entry = Entry(textvariable= code, 
+                  width= 19, bd= 0, 
+                  font= ("arial", 25), 
+                  show= "*")
+    entry.place(x=10, y= 200)
+
+    button1 = Button(text= "ENCRYPT", height= 2, width= 23, 
+                     bg= "#ed3833", fg= "white", bd= 0)
+    button1.place(x= 10, y= 250)
+
+    button2 = Button(text= "DECRYPT", height= 2, width= 23, 
+                     bg= "#00bd56", fg= "white", bd= 0)
+    button2.place(x= 200, y= 250)
 
     screen.mainloop()
 
